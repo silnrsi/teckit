976decc8b683d09529a02eced3754fe248d2a6b6
Neil Mayhew <neil_mayhew@sil.org>
2014-06-30 17:42:36 +0000

    Improve Unicode data update script
     * Use data from a specific version
     * Include NormalizationTest.txt
     * Pass Unicode version on command line
    
    git-svn-id: https://scripts.sil.org/svn/teckit/TRUNK@121 46987711-6ee3-0310-9c75-c0fe504237a4

--- source/UpdateUnidata.sh
+++ source/UpdateUnidata.sh
@@ -2,7 +2,20 @@
 
 # Update the Unicode data that TECKit uses
 
-wget -N http://www.unicode.org/Public/UNIDATA/{CompositionExclusions,UnicodeData}.txt
+if [ $# -ne 1 ]; then
+	echo "Usage: $(basename $0) VERSION" >&2
+	exit 1
+fi
+
+VERSION=$1
+
+BASE=http://www.unicode.org/Public/$VERSION/ucd
+
+wget -N $BASE/{CompositionExclusions,UnicodeData}.txt
 
 perl MakeNormData.pl >NormalizationData.c
 perl MakeUnicodeNames.pl >UnicodeNames.cpp
+
+cd ../test
+
+wget -N $BASE/NormalizationTest.txt
