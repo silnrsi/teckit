15ee9fd4e1a7b24a0687043fd3bab882ad440c9b
Bobby de Vos <bobby_devos@sil.org>
2016-09-07 16:27:14 -0600

    Automate the building of a Windows package

---
+++ build-windows-package.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# This is how the Windows package for release are built.
+
+# Any existing TECkit-<version> folder TECkit-<version>.zip will be deleted.
+
+# metadata
+version=$(git describe)
+foldername=TECkit-$version
+filename=${foldername}.zip
+
+# remove old folder and zip file
+rm -rf $filename $foldername
+
+# create folder for distribution
+cp -a teckit-windows-bin $foldername
+cd $foldername
+
+# add files to folder
+
+# runtime
+cp -p /usr/lib/gcc/i686-w64-mingw32/5.3-posix/libgcc_s_sjlj-1.dll .
+cp -p /usr/lib/gcc/i686-w64-mingw32/5.3-posix/libstdc++-6.dll .
+cp -p /usr/i686-w64-mingw32/lib/libwinpthread-1.dll .
+
+# old files
+archive=../../teckit-archive/2.5.1/TECkit_2008-04-04
+cp -p $archive/Samples.zip .
+cp -p $archive/DropTEC.exe .
+cp -p "$archive/TECkit Mapping Editor.exe" .
+
+# documentation
+unix2dos -n ../README README.txt
+mkdir Documentation
+cp -p ../docs/TECkit_Language_2.1.doc.pdf Documentation
+cp -p ../docs/TECkit_version_2.1.doc.pdf Documentation
+mkdir Developers
+cp -p ../docs/Calling_TECkit_from_VB.doc "Developers/Calling TECkit from VB.doc"
+cp -p ../docs/TECkit_Binary_Format.doc.pdf Developers
+
+# code
+mkdir Developers/Public-headers
+cp -p ../source/Public-headers/*.h Developers/Public-headers
+cp -a ../source/Sample-tools Developers
+
+# create zip file from folder
+cd ..
+zip -r $filename $foldername
