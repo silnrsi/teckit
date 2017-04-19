605d3bed855e3fa25451e6012000dd28df8c39df
Nicolas Spalinger <nicolas.spalinger@sil.org>
2017-04-19 21:38:21 +0200

    Fixed autogen logic again.

--- autogen.sh
+++ autogen.sh
@@ -3,19 +3,26 @@ echo "*** Recreating libtool files"
 if [ "$LIBTOOLIZE" = "" ] && [ "`uname`" == "Darwin" ]; then
         if command -v "glibtoolize" > /dev/null; then
             LIBTOOLIZE=glibtoolize
+            $LIBTOOLIZE -f -c;
         elif command -v "libtoolize" > /dev/null; then
             LIBTOOLIZE=libtoolize
+            $LIBTOOLIZE -f -c;
         else
             echo "DIST: line $LINENO: command libtoolize or glibtoolize not found"
             exit 1
         fi
 fi
 
-if test -z $LTIZE; then
-LTIZE="$AUTODIR""$LIBTOOLIZE"
+
+if [ "$LIBTOOLIZE" = "" ] && [ "`uname`" == "Linux" ]; then
+        if command -v "libtoolize" > /dev/null; then
+            LIBTOOLIZE=libtoolize
+            $LIBTOOLIZE -f -c;
+        else
+            echo "DIST: line $LINENO: command libtoolize not found"
+            exit 1
+        fi
 fi
-echo "$LTIZE"
-	$LTIZE -f -c;
 
 echo "*** Recreating aclocal.m4"
 ACLOCAL="$AUTODIR""aclocal"
