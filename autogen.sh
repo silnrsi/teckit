11fe0e90ac9bf6a2cc067b4d48c4db6f55490291
Nicolas Spalinger <nicolas.spalinger@sil.org>
2017-04-19 15:33:17 +0200

    Added logic for OSX's glibtoolize.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 echo "*** Recreating libtool files"
+if [ "$LIBTOOLIZE" = "" ] && [ "`uname`" == "Darwin" ]; then
+        if command -v "glibtoolize" > /dev/null; then
+            LIBTOOLIZE=glibtoolize
+        elif command -v "libtoolize" > /dev/null; then
+            LIBTOOLIZE=libtoolize
+        else
+            echo "DIST: line $LINENO: command libtoolize or glibtoolize not found"
+            exit 1
+        fi
+fi
+
 if test -z $LTIZE; then
-LTIZE="$AUTODIR""libtoolize"
+LTIZE="$AUTODIR""$LIBTOOLIZE"
 fi
 echo "$LTIZE"
 	$LTIZE -f -c;
