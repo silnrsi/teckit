db1e08f3792945d17806eaa767e158ba49133c8b
Bobby de Vos <bobby_devos@sil.org>
2020-05-05 07:35:36 -0600

    Update copyright year

--- source/Sample-tools/version_defs.h
+++ source/Sample-tools/version_defs.h
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------
-Copyright (c) 2008-2019 SIL International. All rights reserved.
+Copyright (c) 2008-2020 SIL International. All rights reserved.
 
 Distributable under the terms of either the Common Public License or the
 GNU Lesser General Public License, as specified in the LICENSING.txt file.
@@ -18,7 +18,7 @@ GNU Lesser General Public License, as specified in the LICENSING.txt file.
 
 // constants used in all the binaries, shouldn't need changing (until next year)
 #define VER_COMPANY_NAME_STR        "SIL International\0"
-#define VER_COPYRIGHT_STR           "Copyright (c) 2002-2019\0"
+#define VER_COPYRIGHT_STR           "Copyright (c) 2002-2020\0"
 #define VER_PRODUCT_NAME_STR        "TECkit basic tools\0"
 
 #ifndef DEBUG
